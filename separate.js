ce0f1fa250d072c02e021b1817fbbcf32be8450c
oholdermonk <orionholdermonk@gmail.com>
2016-06-01 21:12:08 +1200

    Update separate.js

--- separate.js
+++ separate.js
@@ -14,7 +14,7 @@ function runPost(){
         	snapshot.forEach(function(childSnapshot){
         		var childData = snapshot.val();
         		var date = new Date().getMonth();
-        		if(childData.DATE==date){
+        		if(childData.DATE!=date){
         			console.log("BOOM");
                     FB.api(
         				childData.ID,
