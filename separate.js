cd65703fa59b11c5689fcfda933965183e6eddb1
oholdermonk <orionholdermonk@gmail.com>
2016-06-01 21:28:39 +1200

    Update separate.js

--- separate.js
+++ separate.js
@@ -45,3 +45,38 @@ function runPost(){
 	
 	
 }
+
+
+function quickOn(){
+          
+        myDataRef.on("child_added", function(snapshot) {
+	        var words1 = [];
+	        var words2 = [];
+	
+        	snapshot.forEach(function(childSnapshot){
+        		var childData = snapshot.val();
+        		var date = new Date().getMonth();
+        		if(childData.DATE!=date){
+        			console.log("BOOM");
+                    FB.api(
+        				childData.ID,
+        				"POST",
+        				{
+        					"message": "¯\\_(ツ)_/¯",
+        					"place": "204305412928843"
+        				},
+        				function (response) {
+        			      if (response && !response.error) {
+        			        /* handle the result */
+        			        var thing1= new Firebase(myDataRef+snapshot.key());
+        	    			thing1.remove();
+        			      }
+        			    }
+        		    );
+                } else {
+        			console.log("equal");
+        		}
+        		
+        	    });
+            });
+        }
