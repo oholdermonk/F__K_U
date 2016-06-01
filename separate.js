5909d01b92a3d1bfa611527b9736ba24cd7be443
oholdermonk <orionholdermonk@gmail.com>
2016-06-01 21:09:08 +1200

    Create separate.js

---
+++ separate.js
@@ -0,0 +1,47 @@
+var myDataRef = new Firebase('https://fcku.firebaseio.com/');
+
+function runPost(){
+	var test = new Date().getMonth();
+	console.log(test);
+	console.log("COOL");
+	fullOn();
+	function fullOn(){
+          
+        myDataRef.on("child_added", function(snapshot) {
+	        var words1 = [];
+	        var words2 = [];
+	
+        	snapshot.forEach(function(childSnapshot){
+        		var childData = snapshot.val();
+        		var date = new Date().getMonth();
+        		if(childData.DATE==date){
+        			console.log("BOOM");
+                    FB.api(
+        				childData.ID,
+        				"POST",
+        				{
+        					"message": "¯\\_(ツ)_/¯",
+        					"place": "204305412928843"
+        				},
+        				function (response) {
+        			      if (response && !response.error) {
+        			        /* handle the result */
+        			        var thing1= new Firebase(myDataRef+snapshot.key());
+        	    			thing1.remove();
+        			      }
+        			    }
+        		    );
+                } else {
+        			console.log("equal");
+        		}
+        		
+        	    });
+            });
+        }
+	
+	
+	
+	
+	
+	
+}
